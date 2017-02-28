86d05df4ed5456466a71755a5037caac336ac404
wudada1992 <544429676@qq.com>
2017-02-28 16:03:43 +0800

    del

--- demo/游戏作品-提交版-2017-2-28/js/tool.js
+++
@@ -1,6 +0,0 @@
-window.requestAnimFrame = (function() {
-	return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
-		function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
-			return window.setTimeout(callback, 1000 / 60);
-		};
-})();
